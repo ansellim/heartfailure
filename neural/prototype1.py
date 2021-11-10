b5a9ef1ebc244b657c2268872355e3a051019b14
ansellim <29158160+ansellim@users.noreply.github.com>
2021-11-10 15:08:24 +0800

    reduced batch size from 8 to 4

--- neural/prototype1.py
+++ neural/prototype1.py
@@ -14,6 +14,8 @@ from datasets import Dataset
 import re
 import pickle
 
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+
 random.seed(42)
 np.random.seed(42)
 torch.manual_seed(42)
@@ -45,17 +47,18 @@ def tokenize(string):
 
 train_texts,test_texts = [tokenize(sentence) for sentence in train_texts], [tokenize(sentence) for sentence in test_texts]
 
-
 ########### Model training & evaluation ###########
 
 # model_name = "bert-base-uncased"
 model_name = "bionlp/bluebert_pubmed_mimic_uncased_L-24_H-1024_A-16"
-batch_size=8
+batch_size=4 # reduced batch size from 8 to 4 to try to fit GPU memory constraints
 num_labels=2
 max_length =512
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name,num_labels=num_labels)
+model = AutoModelForSequenceClassification.from_pretrained(model_name,num_labels=num_labels)\
+
+model=model.to(device)
 
 train_encodings = tokenizer(train_texts,truncation=True,padding=True,max_length=max_length)
 test_encodings = tokenizer(test_texts,truncation=True,padding=True,max_length=max_length)
@@ -76,18 +79,21 @@ class MyDataset(torch.utils.data.Dataset):
 train_dataset = MyDataset(train_encodings,train_labels)
 test_dataset = MyDataset(test_encodings,test_labels)
 
+# train_dataset = train_dataset.to(device)
+# test_dataset = test_dataset.to(device)
+
 training_args = TrainingArguments(
-    output_dir='./results',             # output directory
-    num_train_epochs=5,                # total number of training epochs
-    per_device_train_batch_size=8,      # batch size per device during training
-    per_device_eval_batch_size=8,      # batch size for evaluation
-    warmup_steps=500,                   # number of warmup steps for learning rate scheduler
-    weight_decay=0.01,                  # strength of weight decay
-    logging_dir='./logs',               # directory for storing logs
-    load_best_model_at_end=True,        # load the best model when finished training
+    output_dir='./results',                         # output directory
+    num_train_epochs=5,                             # total number of training epochs
+    per_device_train_batch_size=batch_size,          # batch size per device during training
+    per_device_eval_batch_size=batch_size,          # batch size for evaluation
+    warmup_steps=500,                                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,                               # strength of weight decay
+    logging_dir='./logs',                           # directory for storing logs
+    load_best_model_at_end=True,                    # load the best model when finished training
     metric_for_best_model='accuracy',
-    logging_steps=100,                  # log & save weights each logging_steps
-    evaluation_strategy="steps",        # evaluate each `logging_steps`
+    logging_steps=100,                              # log & save weights each logging_steps
+    evaluation_strategy="steps",                    # evaluate each `logging_steps`
 )
 
 def compute_metrics(pred):
