9f26f45fda955700ad34aa7d0e0bfaf2a3d8f679
ansellim <29158160+ansellim@users.noreply.github.com>
2021-11-13 18:41:56 +0800

    removed commented-out faulty code meant for training on train and tuning on validation set; same as before, we do CV grid-search for parameters on train+val and tests on test.

--- ml/adaboost_classifier.py
+++ ml/adaboost_classifier.py
@@ -15,11 +15,6 @@ from feature_engineering import get_data
 import pandas as pd
 from sklearn.model_selection import GridSearchCV
 
-# The hypopt library doesn't work...so I'm commenting it out...
-# https://stackoverflow.com/questions/31948879/using-explicit-predefined-validation-set-for-grid-search-with-sklearn
-# https://github.com/cgnorthcutt/hypopt
-# from hypopt import GridSearch
-
 # Timestamp / start script
 start = datetime.now()
 print(f'Script start: {start}\n')
@@ -43,17 +38,11 @@ parameters = {'clf__base_estimator__max_depth':[i for i in range(2,11,2)],
               'clf__n_estimators':[10,50,250,1000],
               'clf__learning_rate':[0.001,0.01,0.1]}
 
-# The hypopt library doesn't work...
-# opt = GridSearch(model = AdaBoostClassifier(base_estimator= DecisionTreeClassifier(),n_estimators=500, random_state=42),
-#                 param_grid = parameters,parallelize=False)
-# opt.fit(train.text, train.y, val.text, val.y, scoring='roc_auc')
-
 grid_clf = GridSearchCV(clf,parameters)
 
 grid_clf.fit(train_and_val.text,train_and_val.y)
 
 # Display test score for optimized parameters
-#print("Test score:",opt.score(test.text,test.y))
 grid_clf.best_estimator_.score(test.text,test.y)
 
 # Save the best model
