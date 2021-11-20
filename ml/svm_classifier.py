90b761d2fc90ad0d7c350c81d6ea9ddeaad06700
Joanne Lee <jlee3702@gatech.edu>
2021-11-21 03:25:23 +0800

    include perf metrics for svm

--- ml/svm_classifier.py
+++ ml/svm_classifier.py
@@ -11,6 +11,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import GridSearchCV
 import joblib
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 
 def run_svm(X_train, y_train, X_val, y_val, X_test, y_test):
     # Fit the model
@@ -26,14 +27,22 @@ def run_svm(X_train, y_train, X_val, y_val, X_test, y_test):
     print("\n\n\n----------Training SVM---------")
 
     # Display best score / best params
-    print(grid_search.best_score_)
-    print(grid_search.best_params_)
+    print("Best Score: ", grid_search.best_score_)
+    print("Best Parameters: ",grid_search.best_params_)
+
+    best_model = grid_search.best_estimator_
 
     # Check on val & test data
-    print("Val data", grid_search.best_estimator_.score(X_val, y_val))
-    print("Test data", grid_search.best_estimator_.score(X_test, y_test))
+    print("Val data", best_model.score(X_val, y_val))
+    print("Test data", best_model.score(X_test, y_test))
+    
+    pred_test = best_model.predict(X_test)
+    print("\n----------Best SVM---------")
+    print(f"Accuracy: {accuracy_score(y_test, pred_test)}")
+    print(f"AUC: {roc_auc_score(pred_test, y_test)}")
+    print(f"F1-score: {f1_score(y_test, pred_test)}")
 
     # Save the best model
-    joblib.dump(grid_search.best_estimator_,"./models/svm_classifier_" + str(datetime.now().strftime("%Y%m%d_%H%M%S")) + "_.pkl")
+    joblib.dump(best_model,"./models/svm_classifier_" + str(datetime.now().strftime("%Y%m%d_%H%M%S")) + "_.pkl")
 
-    return grid_search.best_estimator_
+    return best_model
